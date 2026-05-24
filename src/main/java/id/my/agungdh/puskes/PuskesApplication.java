4c5156513e7847f56e96dc438c7070a64dc9d02d
AgungDH <agungdh@live.com>
2026-05-24 14:11:59 +0700

    init

---
+++ src/main/java/id/my/agungdh/puskes/PuskesApplication.java
@@ -0,0 +1,13 @@
+package id.my.agungdh.puskes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuskesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PuskesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/agungdh/puskes/PuskesApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.agungdh.puskes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuskesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
